201ca2ac8c3e0d84a72635a7699ec0966d359052
Kael Matthews <kaelmatthews@gmail.com>
2019-05-12 12:38:31 +0800

    Install Styled Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,22 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styled from "styled-components"
+
+const Container = styled.div`
+  max-width: 200px;
+`
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Container>
       <Image />
-    </div>
+    </Container>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
