1c0404a549df637f7ed31c1b2737bcd58bb628c6
Kael Matthews <kaelmatthews@gmail.com>
2019-05-13 15:54:56 +0800

    Fix Font Paths

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,6 +1,6 @@
-import BlackoutTTF from '@fonts/blackout/blackout.ttf';
-import BlackoutWOFF from '@fonts/blackout/blackout.ttf';
-import BlackoutWOFF2 from '@fonts/blackout/blackout.ttf';
+import BlackoutTTF from '@fonts/blackout/Blackout.ttf';
+import BlackoutWOFF from '@fonts/blackout/Blackout.woff';
+import BlackoutWOFF2 from '@fonts/blackout/Blackout.woff2';
 
 export {
     BlackoutTTF,
